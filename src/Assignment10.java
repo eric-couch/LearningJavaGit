d7e818b4cabdf90ff81b5122ab2580a5cf425b1b
eric-couch <James.Couch@edgetechacademy.edu>
2019-10-28 21:59:24 -0500

    added to assignment 10

--- src/Assignment10.java
+++ src/Assignment10.java
@@ -7,15 +7,26 @@ public class Assignment10 {
             deliveryCost = 20.0f;
             if (orderValue > 100.00f) {
                 deliveryCost -= 5.00f;
-                // deliveryCost = deliveryCost - 5.00f;
             }
             if (saturdayDelivery) {
                 deliveryCost += 10.00f;
             }
         } else if ( destination.equals("Mexico") ) {
             deliveryCost = 32.0f;
+            if (orderValue > 100.00f) {
+                deliveryCost -= 7.00f;
+            }
+            if (saturdayDelivery) {
+                deliveryCost += 20.00f;
+            }
         } else {      // Assume that the country is Canada
             deliveryCost = 25.0f;
+            if (orderValue > 100.00f) {
+                deliveryCost -= 7.00f;
+            }
+            if (saturdayDelivery) {
+                deliveryCost += 12.00f;
+            }
         }
 
         return deliveryCost;
@@ -24,5 +35,8 @@ public class Assignment10 {
     public static void main(String[] args) {
         System.out.println(shipping(200.0f,true,"Canada"));
         System.out.println(shipping(100.0f,false,"Mexico"));
+        System.out.println(shipping(50.0f,true,"US"));
+        System.out.println(shipping(600.0f,false,"US"));
+        System.out.println(shipping(100.0f,false,"Canada"));
     }
 }
